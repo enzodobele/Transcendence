8b84eb8b3bb180b0b2f1a2c63918607f8508e453
MSI portable <enzo.dobele@yahoo.fr>
2026-06-18 17:23:28 +0200

    Enzo: backend auth fixed

--- src/frontend/src/contexts/AuthContext.tsx
+++ src/frontend/src/contexts/AuthContext.tsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useState, useEffect} from "react";
 import type { ReactNode } from "react";
 
 interface User {
-  id: Number;
+  id: number;
   email: string;
   username: string;
 }
